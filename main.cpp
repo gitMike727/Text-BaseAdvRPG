d9988e65bbaec26dd3a4547aaf89623ab69158e0
mtruong727 <mtruong727@outlook.com>
2025-04-14 13:53:53 -0500

    Added game locations

--- main.cpp
+++ main.cpp
@@ -27,9 +27,32 @@ int main()
     std::cout << "\n";
 
     std::cout << "Welcome, " << playerName << ", to the Dragon Realm!\n";
-
+    std::cout << "-----------------------------------------\n";
+   
     int choice;
 
-    
+    std::cout << "Where will you go, " << playerName << "?\n";
+    std::cout << "1. Dragon Town\n";
+    std::cout << "2. Shimmering Lake\n";
+    std::cout << "3. Green Forest\n";
+    std::cout << "Please enter your choice(1-3): \n";
+    std::cin >> choice;
+
+    if (choice == 1) {
+        std::cout << "You chose Dragon Town.\n";
+    }
+    else if (choice == 2) {
+        std::cout << "You chose Shimmering Lake.\n";
+    }
+    else if (choice == 3) {
+        std::cout << "You chose Green Forest.\n";
+    }
+    else {
+        std::cout << "***INVALID INPUT***\n";
+    }
+
+    switch(choice) {
+        
+    }
 
 }
