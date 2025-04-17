8e36f2f8a952620aa0439407bd0be339f1baf991
mtruong727 <mtruong727@outlook.com>
2025-04-17 16:53:23 -0500

    Add Shimmering Lake content & nestedChoice variable

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ int main()
     std::cout << "-----------------------------------------\n";
    
     int choice;
+    int nestedChoice;
 
     std::cout << "Where will you go, " << playerName << "?\n";
     std::cout << "1. Dragon Town\n";
@@ -38,21 +39,37 @@ int main()
     std::cout << "Please enter your choice(1-3): \n";
     std::cin >> choice;
 
-    if (choice == 1) {
-        std::cout << "You chose Dragon Town.\n";
-    }
-    else if (choice == 2) {
-        std::cout << "You chose Shimmering Lake.\n";
-    }
-    else if (choice == 3) {
-        std::cout << "You chose Green Forest.\n";
-    }
-    else {
-        std::cout << "***INVALID INPUT***\n";
-    }
+    // if (choice == 1) {
+    //     std::cout << "You chose Dragon Town.\n";
+    // }
+    // else if (choice == 2) {
+    //     std::cout << "You chose Shimmering Lake.\n";
+    // }
+    // else if (choice == 3) {
+    //     std::cout << "You chose Green Forest.\n";
+    // }
+    // else {
+    //     std::cout << "***INVALID INPUT***\n";
+    // }
 
     switch(choice) {
-        
+            case 1:
+                std::cout << "You chose Dragon Town.\n";
+                break;
+            case 2:
+                std::cout << playerName << " arrives at the Shimmering Lake. It is one of the most\n" <<
+                "largest lakes that " << playerName << " has seen. " << playerName << " hears a mysterious melody\n" <<
+                "from the water. They can either: \n";  
+                std::cout << "1. Stay quiet and listen.\n";
+                std::cout << "2. Sing along with the melody.\n";
+                std::cout << "Please enter your choice: \n";
+                break;
+            case 3:
+                std::cout << "You chose Green Forest.\n";
+                break;
+            default:
+                 std::cout << "***INVALID INPUT***\n";
+
     }
 
 }
